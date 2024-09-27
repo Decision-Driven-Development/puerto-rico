532bc78c35fc6a399dae3bf48bee70e94e30a96a
Евгений Терехов <nergal@psu.ru>
2024-09-27 09:14:23 +0400

    feat(settler): moving plantations from board to drop at the end of the settler phase

--- functions/plantation-next.groovy
+++ functions/plantation-next.groovy
@@ -1,7 +1,24 @@
-def indices = context.valueFor('temp', 'plantations')
-        .replaceAll("\\[|\\]", '')
-        .split(',')
-        .collect { it.toInteger() }
-def first = indices.first()
-context.setValueFor('temp', 'plantations', indices.drop(1).toString())
-first
+def stackPlantations = []
+def dropPlantations = []
+
+(1..50).each { i ->
+    def owner = context.valueFor('plantation'+i,'owner')
+    if (owner == 'stack') {
+        stackPlantations << i
+    } else if (owner == 'drop') {
+        dropPlantations << i
+    }
+}
+
+println "Stack: ${stackPlantations}"
+println "Drop: ${dropPlantations}"
+
+if (stackPlantations.size() > 0) {
+    return stackPlantations.shuffled().first()
+}
+
+if (dropPlantations.size() > 0) {
+    return dropPlantations.shuffled().first()
+}
+
+return 'undefined'
--- functions/plantations-shuffle.groovy
+++
@@ -1,6 +0,0 @@
-// write code that creates randomly shuffled list of numbers from 1 to 50
-// and returns it
-
-def list = (1..50).toList()
-list.shuffle()
-return list
